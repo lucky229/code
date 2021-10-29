e58a5bdac740b4b8ade31eaf332c0bffc54f502e
lucky229 <8780037@qq.com>
2021-10-29 20:19:47 +0800

    1029

---
+++ cpp/joseph.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int jose[100];
+
+int main()
+{
+	int n, k, m, num, t;
+	cin>>n>>k>>m;
+	
+	for(int i=0; i<n; i++)
+		jose[i] = 1;
+	
+	t = m;
+	num = 0;
+	
+	
+	while()
+	{
+		if
+		if(jose[t] != 1)
+			num++;
+	}
+	
+	return 0;
+}
