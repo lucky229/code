75870c09ea227629406fe6e45bce69fce87edf24
lucky229 <8780037@qq.com>
2022-04-08 16:30:54 +0800

    20220408

--- cpp/bijaodx.cpp
+++ cpp/bijaodx.cpp
@@ -2,40 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[1024][1024];
-
-void solve(int k, int x, int y, int v)
-{
-	if(k==0)
-	{
-		a[x][y] = v;
-	}
-	else
-	{
-		int m = 1<<k-1;
-		solve(k-1, x, y, v);
-		solve(k-1, x, y+m, v+m);
-		solve(k-1, x+m, y, v+m);
-		solve(k-1, x+m, y+m, v);
-	}
- } 
 
 int main()
 {
-	int n, k;
-	scanf("%d", &k);
-	n = 1<<k;
+	int m, n;
 	
-	solve(k, 1, 1, 1);
+	//m = pow(3, 3);
 	
-	for(int i=1; i<=n; i++)
-	{
-		for(int j=1; j<=n; j++)
-		{
-			printf("%d ", a[i][j]);
-		}
-		printf("\n");
-	}
+	n = 3* log10(3) + 1;
+	
+	cout<<n;
+
 	
     return 0;
 }
--- cpp/zuixiaoshu.cpp
+++ cpp/zuixiaoshu.cpp
@@ -1,31 +1,64 @@
+// ¥∫ºæ—µ¡∑”™2£∫ I ∆Â≈Ã∏≤∏«Œ Ã‚ 
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-queue<int>b,c;
+
+int k, xx, yy, n, id=0, id2=0;
+int new_id[1025*1025];
+int aa[1030][1030];
+int dx[4] = {0, 0, 1, 1}, dy[4] = {0, 1, 0, 1};
+
+void solve(int k, int x, int y, int dir)
+{
+	int m = k / 2;
+	int d = dir >= 0 ? dir : (xx >= x + m) * 2 + (yy >= y + m);
+	if(k == 2)
+	{
+		++id;
+		for(int i = 0; i < 4; i++)
+		{
+			if(d != i)
+			{
+				aa[x + dx[i]][y + dy[i]] = id;
+			}
+		}
+	}
+	else
+	{
+		for(int i = 0; i < 4; i++)
+		{
+			if(d != i)
+			{
+				solve(m, x + dx[i] * m, y + dy[i] * m, 3 - i);
+			}					
+		}
+		solve(m, x + m / 2, y + m / 2, d);
+		if(dir < 0)
+		{
+			solve(m, x + dx[d] * m, y + dy[d] * m, -1);
+		}
+	}	
+} 
  
 int main()
 {
-	scanf("%d", &n);
-	int x = 1;
-	for(int i=1;i<=n;i++)
+	ios::sync_with_stdio(false);
+	
+	cin>>k>>xx>>yy;
+	n = 1<<k;
+	solve(n, 1, 1, -1);
+	for(int i=1; i<=n; i++)
 	{
-		printf("%d ", x);
-		b.push(2 * x + 1);
-		c.push(3 * x + 1);
-		if(b.front()<c.front())
+		for(int j=1; j<=n; j++)
 		{
-			x = b.front();
-			b.pop();
+			if(aa[i][j]&&new_id[aa[i][j]]==0)
+			{
+				++id2;
+				new_id[aa[i][j]] = id2;
+			}
+			cout<<new_id[aa[i][j]]<<" ";
+			//cout<<aa[i][j]<<" ";
 		}
-		else
-		{
-			x = c.front();
-			c.pop();
-		}
-		if(b.front() == c.front())
-		{
-			b.pop();
-		}
+		cout<<endl;
 	}
 	
 	return 0;
