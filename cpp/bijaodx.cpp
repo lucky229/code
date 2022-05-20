0231ebb29b6a95d41830e999a004b4503712c6e2
lucky229 <8780037@qq.com>
2022-05-20 15:39:05 +0800

    20220520

--- cpp/bijaodx.cpp
+++ cpp/bijaodx.cpp
@@ -1,112 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int r,n,c,cnt=0;
-int x,xx,y,yy;
-stack<int> s;
-struct gg{
-	int x,y,p,id;
-}xa[200010];
-bool check(int a,int b,int aa,int bb)
-{
-	if((a==0||a==r||b==0||b==c)&&(aa==0||aa==r||bb==0||bb==c))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-int chec(int x,int y)
-{
-	if(x==0)
-	{
-		return 1;
-	}
-	if(y==c)
-	{
-		return 2;
-	}
-	if(x==r)
-	{
-		return 3;
-	}
-	if(y==0)
-	{
-		return 4;
-	}
-}
-bool cmp(gg a,gg b)
-{
-	if(a.p==b.p)
-	{
-		if(a.p==1)
-		{
-			return a.y<b.y;
-		}
-		if(a.p==2)
-		{
-			return a.x<b.x;
-		}
-		if(a.p==3)
-		{
-			return a.y>b.y;
-		 } 
-		if(a.p==4)
-		{
-			return a.x>b.x;
-		}
-	}
-	else
-	{
-		return a.p<b.p;
-	}
-}
+
+int n, m, x, y;
+int f[30];
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin>>r>>c>>n;
-	for(int i=1;i<=n;i++)
+	scanf("%d%d%d%d", &n, &m, &x, &y);
+	f[0] = 1;
+	for(int i=0; i<=n; i++)
 	{
-		cin>>x>>y>>xx>>yy;
-		if(check(x,y,xx,yy)==true)
+		for(int j=0; j<=m; j++)
 		{
-			//cout<<i<<endl;
-			cnt++;
-			xa[cnt].x=x;
-			xa[cnt].y=y;
-			xa[cnt].id=i;
-			//cout<<"cnt:"<<cnt<<" "<<xa[cnt].id<<endl;
-			xa[cnt].p=chec(x,y);
-			cnt++;
-			xa[cnt].x=xx;
-			xa[cnt].y=yy;
-			xa[cnt].id=i;
-			//cout<<"cnt:"<<cnt<<" "<<xa[cnt].id<<endl;
-			xa[cnt].p=chec(xx,yy);
+			bool xy = (i==x&&j==y)||(i==x+1&&(j==y+2||j==y-2))||(i==x+2&&(j==y+1||j==y-1))||(i==x-1&&(j==y+2||j==y-2))||(i==x-2&&(j==y+1||j==y-1));
+			if(!xy)
+			{
+				if(j!=0)
+				{
+					f[j] = f[j-1] + f[j];
+				}				
+			}
+			else
+			{
+				f[j] = 0;
+			}
+			//cout<<i<<" -- "<<j<<"  :  "<<f[j]<<endl;
 		}
 	}
-	//cout<<cnt; 
-	sort(xa+1,xa+1+cnt,cmp);
-	for(int i=1;i<=cnt;i++)
-	{
-		if(!s.empty()&&s.top()==xa[i].id)
-		{
-			s.pop();
-		}
-		else
-		{
-			s.push(xa[i].id);
-		}
-		//cout<<i<<": "<<xa[i].id<<endl;
-	}
-	if(s.empty())
-	{
-		cout<<"YES";
-	}
-	else
-	{
-		cout<<"NO";
-	}
+	printf("%d", f[m]);
 	return 0;
- }
+}
