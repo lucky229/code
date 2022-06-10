98abda0adc2a963ace3d869e90e2c66164da7742
lucky229 <8780037@qq.com>
2022-06-10 15:21:42 +0800

    20220610

--- cpp/bijaodx.cpp
+++ cpp/bijaodx.cpp
@@ -1,43 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-int x,y,z;
-int a[210],b[210],c[210];
-int xx[210],xy[210];
-int f[210][210];
-bool d[210];
+long long n,ans=LLONG_MAX,minn;
+long long a[5010],sum[5010]; 
+long long f[5010][5010];
 int main()
 {
-	scanf("%d%d%d",&x,&y,&z);
-	for(int i=1;i<=x;i++)
+	scanf("%lld",&n);
+	for(long long i=1;i<=n;i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%lld",&a[i]);
+		sum[i]=sum[i-1]+a[i];
 	}
-	for(int i=1;i<=y;i++)
+	for(int j=1;j<=n;j++)
 	{
-		scanf("%d",&b[i]);
-	}
-	for(int i=1;i<=z;i++)
-	{
-		scanf("%d",&c[i]);
-	}
-	for(int i=1;i<=x;i++)
-	{
-		for(int j=1;j<=y;j++)
+		for(int i=1;i+j<=1+n;i++)
 		{
-			for(int k=1;k<=z;k++)
+			if(j==1)
 			{
-				if(a[i]==b[j]&&b[j]==c[k])
+				f[i][j]=0;
+			}
+			else
+			{
+				for(int k=1;k<j;k++)
 				{
-					f[j][k]=f[j][k-1]+1;
+					if(k==1)
+					{
+						minn=LLONG_MAX;
+						//cout<<")(*";
+					}
+					//minn=min(minn,f[i][i+k-1]+f[i+k][j+i-1]);
+					minn=min(minn,f[i][k]+f[i+k][j-k]);
+			//cout<<f[i][i+k-1]+f[i+k][j+i-1]<<endl;
+
 				}
-				else
-				{
-					f[j][k]=max(f[j][k],max(f[j][k-1],f[j-1][k]));
-				} 
-				cout<<i<<" -- "<<j<<" -- "<<k<<" : "<<f[j][k]<<endl;
-			}				
-		}
+				f[i][j]=sum[i+j-1]-sum[i-1]+minn;
+			}
+			cout<<f[i][j]<<" ";
+		} 
+		cout<<endl;
 	}
-	printf("%d",f[y][z]);
+	ans=min(ans,f[1][n]);
+	cout<<f[1][n];
 	return 0;
 }
